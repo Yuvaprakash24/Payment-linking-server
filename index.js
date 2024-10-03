306feeb8ecad19e281c1dc9e6c1acf16cb48b2cf
Yuvaprakash24 <114161768+Yuvaprakash24@users.noreply.github.com>
2024-10-03 22:43:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.post('/create-checkout-session', async(req, res) => {
             payment_method_types: ['card'],
             line_items,
             mode: 'payment',
-            success_url: "http://localhost:5173/success",
-            cancel_url: "http://localhost:5173/cancel",
+            success_url: "https://yuvis-ez-foods.netlify.app/success",
+            cancel_url: "https://yuvis-ez-foods.netlify.app/cancel",
         });
         res.json({id: session.id});
     } catch(error){
